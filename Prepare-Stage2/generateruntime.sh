8f3bf08c89c722f5aa5cedb3b9a9af116a1acac2
Bernard Sufrin <Bernard.Sufrin@cs.ox.ac.uk>
2026-05-31 18:14:31 +0100

    Generating scalalr and scalalrruntime corrected

--- Prepare-Stage2/generateruntime.sh
+++ Prepare-Stage2/generateruntime.sh
@@ -3,8 +3,8 @@
 sync=""; read -p "Build the components first? [y for yes]" sync
 [ "$sync" = "y" ] && sh preparestage2-sh.sh
 
-SCALA=stage2.scala
-JAR=scalalr.jar
+SCALA=scalalrruntime.scala
+JAR=scalalrruntime.jar
 
 sync=""; read -p "Make the fat jar $PROGRAM? [ENTER for yes]" sync
 [ "$sync" = "" ] && scala-cli --power package $SCALA -o $JAR --assembly --preamble=false -f
